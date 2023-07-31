1a2e3d7529f83900f2a06c48bb35c4c97b8e5ab4
Darshan-V <mr.darshanv@gmail.com>
2023-07-31 10:34:33 +0530

    basic nest app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BudgetsModule } from './budgets/budgets.module';
+import { CategoryGroupsModule } from './category-groups/category-groups.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Budgets } from './budgets/entities/budgets.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'budget_tracker',
+      entities: [Budgets],
+      // synchronize: true,
+    }),
+    BudgetsModule,
+    CategoryGroupsModule,
+    UsersModule,
+    CategoriesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
