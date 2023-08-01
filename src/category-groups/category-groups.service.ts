add14edf927ca08dd4afa3309935e5ff3376eb50
Darshan-V <mr.darshanv@gmail.com>
2023-08-01 18:07:57 +0530

    init service with create function

--- src/category-groups/category-groups.service.ts
+++ src/category-groups/category-groups.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryGroupDto } from './dto/create-category-group.dto';
 import { UpdateCategoryGroupDto } from './dto/update-category-group.dto';
+import { CategoryGroups } from './entities/category-groups.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CategoryGroupsService {
-  create(createCategoryGroupDto: CreateCategoryGroupDto) {
-    return 'This action adds a new categoryGroup';
+  constructor(
+    @InjectRepository(CategoryGroups)
+    private categoryGroupRepository: Repository<CategoryGroups>,
+  ) {}
+
+  async create(
+    createCategoryGroupDto: CreateCategoryGroupDto,
+  ): Promise<CategoryGroups> {
+    const group = this.categoryGroupRepository.create(createCategoryGroupDto);
+    return this.categoryGroupRepository.save(group);
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all categoryGroups`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} categoryGroup`;
   }
 
-  update(id: number, updateCategoryGroupDto: UpdateCategoryGroupDto) {
+  async update(id: number, updateCategoryGroupDto: UpdateCategoryGroupDto) {
     return `This action updates a #${id} categoryGroup`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} categoryGroup`;
   }
 }
