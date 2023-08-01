26491a30b776704fab7ad2985fa0f28de93a921f
Darshan-V <mr.darshanv@gmail.com>
2023-08-01 18:06:09 +0530

    init module for category-groups

--- src/category-groups/category-groups.module.ts
+++ src/category-groups/category-groups.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CategoryGroupsService } from './category-groups.service';
 import { CategoryGroupsController } from './category-groups.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryGroups } from './entities/category-groups.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoryGroups])],
   controllers: [CategoryGroupsController],
-  providers: [CategoryGroupsService]
+  providers: [CategoryGroupsService],
 })
 export class CategoryGroupsModule {}
