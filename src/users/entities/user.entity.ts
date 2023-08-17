ccd87370d4e64601f2bab12c7feb008f23c077fe
Darshan-V <mr.darshanv@gmail.com>
2023-08-17 22:31:21 +0530

    init file

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { PrimaryGeneratedColumn, Entity, Column } from 'typeorm';
+
+Entity();
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  role: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+}
