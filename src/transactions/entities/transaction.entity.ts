d691ff18e7b81fc56376d8eeee26cb32a74213c2
Darshan-V <mr.darshanv@gmail.com>
2023-08-17 22:31:05 +0530

    udpate transactions entity

--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,25 @@
-export class Transaction {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+Entity();
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  payee: string;
+
+  @Column()
+  memo: string;
+
+  @Column()
+  inflow: number;
+
+  @Column()
+  outflow: number;
+
+  @Column()
+  transaction_ref: number;
+
+  @Column()
+  transaction_id: number;
+}
